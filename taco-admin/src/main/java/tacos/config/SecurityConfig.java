7707b7f3ad6d7747cc30d7d81368cda53b7329cd
Nguyen Trung Nghia <127302636+neekon2508@users.noreply.github.com>
2025-01-14 07:50:03 +0700

    Reset client project

--- taco-admin/src/main/java/tacos/config/SecurityConfig.java
+++ taco-admin/src/main/java/tacos/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class SecurityConfig {
@@ -21,8 +23,5 @@ public class SecurityConfig {
     .oauth2Client(Customizer.withDefaults());
   return http.build();
  }
- 
-
-
 
 }
